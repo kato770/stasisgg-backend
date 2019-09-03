d4b0907bc46006b7d0926da3b8dadae0d1614515
Ryo Nakatani <right.rcon.f@outlook.com>
2019-09-03 14:34:47 +0900

    Add test case for queueHelper

--- src/__test__/queueHelper.test.ts
+++ src/__test__/queueHelper.test.ts
@@ -2,11 +2,14 @@ import { getMapFromQueueId } from '../queueHelper';
 
 
 describe('getMapFromQueueId', () => {
+  it('return Ranked', () => {
+    expect(getMapFromQueueId(420)).toBe('Ranked');
+  });
   it('return Normal', () => {
     expect(getMapFromQueueId(430)).toBe('Normal');
   });
-  it('return Ranked', () => {
-    expect(getMapFromQueueId(420)).toBe('Ranked');
+  it('return Flex', () => {
+    expect(getMapFromQueueId(440)).toBe('Flex');
   });
   it('return error', () => {
     expect(() => {
