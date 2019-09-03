ba2a976fb2350579acfdb19a5b0448da2231ae44
Ryo Nakatani <right.rcon.f@outlook.com>
2019-09-03 14:33:35 +0900

    Add some test case for queueHelper

--- src/__test__/queueHelper.test.ts
+++ src/__test__/queueHelper.test.ts
@@ -5,4 +5,12 @@ describe('getMapFromQueueId', () => {
   it('return Normal', () => {
     expect(getMapFromQueueId(430)).toBe('Normal');
   });
+  it('return Ranked', () => {
+    expect(getMapFromQueueId(420)).toBe('Ranked');
+  });
+  it('return error', () => {
+    expect(() => {
+      getMapFromQueueId(999);
+    }).toThrow(new Error(`queueId: 999 is not supported`));
+  });
 });
