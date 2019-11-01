0bc3361b503870fc1639266d86956555cbe71605
Ryo Nakatani <right.rcon.f@outlook.com>
2019-11-01 14:27:54 +0900

    Add get-matches endpoint (no test yet)

---
+++ src/get-matches/getMatches.ts
@@ -0,0 +1,71 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { REGIONS } from "kayn";
+import { kayn } from "..//helper/intializeKayn";
+import { makeErrorResponse, makeResponse } from "../helper/responseBuilder";
+
+export const getMatches = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  if (
+    event.queryStringParameters === null ||
+    !event.queryStringParameters.summonerName ||
+    !event.queryStringParameters.region
+  ) {
+    return makeErrorResponse(
+      400,
+      "summonerName and region parameters are required."
+    );
+  }
+  const region = event.queryStringParameters.region;
+  if (!Object.values(REGIONS).find(value => value === region)) {
+    return makeErrorResponse(
+      400,
+      `${region} must be one of ${Object.values(REGIONS)}`
+    );
+  }
+
+  const summonerName = event.queryStringParameters.summonerName;
+  const { accountId } = await kayn.Summoner.by
+    .name(summonerName)
+    .region(region);
+  //console.log(`name: ${name}\naccountId: ${accountId}`);
+  if (accountId === undefined) {
+    return makeErrorResponse(
+      404,
+      `Summoner Name: ${summonerName} was not found.`
+    );
+  }
+
+  const { matches } = await kayn.Matchlist.by
+    .accountID(accountId)
+    .query({ queue: [420, 430] })
+    .region(region);
+  if (matches === undefined) {
+    return makeErrorResponse(404, "No games.");
+  }
+
+  const offset = +event.queryStringParameters.offset || 0;
+  const limit = +event.queryStringParameters.limit || 10;
+  // get non-null game ids
+  const nonNullableGameIds: number[] = matches
+    .slice(offset, offset + limit)
+    .map(({ gameId }) => gameId)
+    .filter((item: number | undefined): item is number => item !== null);
+
+  if (nonNullableGameIds.length === 0) {
+    return makeErrorResponse(404, "No gameIds.");
+  }
+  const requests = nonNullableGameIds.map(
+    async id => await kayn.Match.get(id).region(region)
+  );
+  const results = await Promise.all(requests);
+
+  const responseBody = {
+    matchesCount: nonNullableGameIds.length,
+    matchIds: nonNullableGameIds,
+    macthes: results
+  };
+
+  // TODO: make response more useful
+  return makeResponse(200, event.queryStringParameters, responseBody);
+};
--- src/helper/intializeKayn.ts
+++ src/helper/intializeKayn.ts
@@ -1,33 +1,33 @@
-require('dotenv').config();
-import { Kayn, REGIONS, LRUCache, METHOD_NAMES } from 'kayn';
+require("dotenv").config();
+import { Kayn, REGIONS, LRUCache, METHOD_NAMES } from "kayn";
 
-const RIOT_LOL_API_KEY = process.env.RIOT_LOL_API_KEY || 'dummyAPIKey';
+const RIOT_LOL_API_KEY = process.env.RIOT_LOL_API_KEY || "dummyAPIKey";
 const lruCache = new LRUCache({ max: 500 });
 
 export const kayn = Kayn(RIOT_LOL_API_KEY)({
   region: REGIONS.KOREA,
   debugOptions: {
     isEnabled: true,
-    showKey: false,
+    showKey: false
   },
   requestOptions: {
     shouldRetry: true,
     numberOfRetriesBeforeAbort: 3,
     delayBeforeRetry: 1,
-    burst: false,
-    shouldExitOn403: false,
+    burst: true,
+    shouldExitOn403: false
   },
   cacheOptions: {
     cache: lruCache,
     timeToLives: {
       useDefault: true,
       byGroup: {
-        MATCH: 60 * 60 * 24 * 30,
+        MATCH: 60 * 60 * 24 * 30
       },
       byMethod: {
         [METHOD_NAMES.SUMMONER.GET_BY_SUMMONER_NAME_V4]: 1000,
         [METHOD_NAMES.MATCH.GET_MATCHLIST_V4]: 1000 * 60 * 5
-      },
-    },
-  },
+      }
+    }
+  }
 });
